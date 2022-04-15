5e928ed6c44d9a032d7e62efdb544e2713db6cea
Maria <marialshy@gmail.com>
2022-04-15 19:38:18 +0300

    practiсe - numbers from 0 to N, %2

---
+++ seminar1_hw8/Program.cs
@@ -0,0 +1,10 @@
+﻿// все четные числа от 1 до N, где N введенное
+Console.WriteLine("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (count<=N)
+{
+Console.Write(count);
+Console.Write(", ");
+count +=2;
+}
