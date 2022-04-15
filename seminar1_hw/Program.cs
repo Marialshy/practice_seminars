23024d3e398dc3e288211784ae7170caaf2e6f71
Maria <marialshy@gmail.com>
2022-04-15 18:04:32 +0300

    practice1 - compare 2 numbers

---
+++ seminar1_hw/Program.cs
@@ -0,0 +1,13 @@
+﻿// из двух введенных числе выбирает большее
+Console.WriteLine("введите число 1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1>number2)
+{
+    Console.WriteLine("max = "+ number1 +" ");
+}
+else
+{
+    Console.WriteLine("max = "+ number2 +" ");
+}
