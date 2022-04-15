b8f40114cf5b540aeec577ce52986f10b59e1401
Maria <marialshy@gmail.com>
2022-04-15 19:49:47 +0300

    practiсe - if/else max of 3 numbers

---
+++ seminar1_hw4/Program.cs
@@ -0,0 +1,28 @@
+﻿// из трех введенных числе выбирает наибольшее
+Console.WriteLine("введите число 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 3");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (b > max)
+{
+    max = b;
+    if (c > max) 
+    {
+        max = c;
+        Console.WriteLine(max);
+    }
+    else Console.WriteLine(max);
+}
+else if (c > max)
+{
+    max = c;
+    Console.WriteLine(max);
+}
+else
+Console.WriteLine(max);
+
+    
