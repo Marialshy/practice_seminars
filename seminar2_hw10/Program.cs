3f2b4d04ce2735a99ae500c65123f55ca5321927
Maria <marialshy@gmail.com>
2022-04-17 15:07:39 +0300

    practiсe - 2nd from 3-digit number

---
+++ seminar2_hw10/Program.cs
@@ -0,0 +1,11 @@
+﻿// для введенного трехзначного числа показывает вторую цифру
+Console.WriteLine("введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1000 && n > 99)
+{
+    int result = (n%100 - n%10)/10;
+    Console.WriteLine("вторая цифра вашего числа: "+ result +"");
+}
+else Console.WriteLine("число должно быть трехначным");
+
+
