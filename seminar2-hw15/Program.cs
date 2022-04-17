a66d4c80ec441d80f91a83a9307131c9d1615043
Maria <marialshy@gmail.com>
2022-04-17 19:12:30 +0300

    practiсe - weekdays working or not

---
+++ seminar2-hw15/Program.cs
@@ -0,0 +1,9 @@
+﻿// по введенной цифре вывести ответ, выходной ли этот день недели
+Console.WriteLine("введите день недели в числовом формате от 1 до 7");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>0 && n<=7)
+{
+    if (n==6 || n==7) Console.WriteLine("выходной");
+    else Console.WriteLine("будний");
+}
+else Console.WriteLine("введите число от 1 до 7");
