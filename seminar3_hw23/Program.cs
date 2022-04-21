d62d5712c8da8d9080625de59ebb681d81cc3e87
Maria <marialshy@gmail.com>
2022-04-21 19:32:39 +0300

    practice -print for some number n cube-table: from 1 till n

---
+++ seminar3_hw23/Program.cs
@@ -0,0 +1,17 @@
+﻿// вывести кубы чисел от 1 до n, полученного с клавиатуры
+int getCube (int a)
+{
+    int ax3=a*a*a;
+    return ax3;
+}
+
+Console.WriteLine("enter number");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count<=n) 
+{
+    int nx3 = getCube (count);
+    Console.Write(""+ nx3 +"; ");
+    count ++;
+}
