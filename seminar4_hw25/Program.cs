1650709a88962fd398075a88081309d0d5331205
Maria <marialshy@gmail.com>
2022-04-27 20:53:34 +0300

    seminar 4 - to count exponential, TryParce, to count sum of digit for N

---
+++ seminar4_hw25/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("Please enter number");
+int n = Convert.ToInt32(Console.ReadLine());
+int exponential;
+Console.WriteLine("please enter exponential to count");
+while (!int.TryParse (Console.ReadLine(), out exponential) || exponential <=0){
+    Console.WriteLine("Invalid input. Please enter positive integer number");
+}
+int result=1;
+for(int j=1; j<=exponential; j++){
+    result=result*n;
+}
+Console.WriteLine(result);
---
+++ seminar4_hw27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int n;
+Console.WriteLine("Please enter positive integer number");
+while (!int.TryParse (Console.ReadLine(), out n) || n <=0){
+    Console.WriteLine("Invalid input. Please enter positive integer number");
+}
+int sumN (int a) 
+{
+   int result = 0;
+   while(a>=1){
+       result = result+a%10;
+       a=a/10;
+    }
+    return result;
+}
+Console.Write("-> "+sumN(n));
