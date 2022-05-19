4aca443276f89dbd8ad9c66b264fdc4f8ffcb2fb
Maria <marialshy@gmail.com>
2022-05-19 19:08:27 +0300

    array sorting

---
+++ seminar8_hw54/Program54.cs
@@ -0,0 +1,78 @@
+﻿// 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.WriteLine("for your new array: m strings, n coloumns,");
+int m = CheckInputInt("m");
+int n = CheckInputInt("n");
+int[,] arr = new int[m, n];
+
+FillArray(arr, -10, 10);
+PrintArray(arr);
+
+ArrayToArrange(arr);
+Console.WriteLine();
+PrintArray(arr);
+
+void FillArray(int[,] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(min, max);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int CheckInputInt(string a)
+{
+    int n;
+    Console.WriteLine($"please enter positive integer number for {a}");
+    while (!int.TryParse(Console.ReadLine(), out n) || n <= 0) // проверка n на тип данных (int)
+        Console.WriteLine("Invalid input. Please positive integer enter number");
+    return n;
+}
+
+void ArrayToArrange(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+/*void ArraySort(int[] array) //пузырьковая сортировка для одномерного массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length - 1; j++)
+        {
+            if (array[j] < array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+*/
+
